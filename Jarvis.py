c4e2f2ca99aabaa349f9451e14434b184f367266
keatonminor <54481238+keatonminor@users.noreply.github.com>
2023-07-02 15:48:13 -0500

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -7,7 +7,7 @@ engine.setProperty('rate', 160)
 engine.setProperty('pitch', 0.8)
 recognizer = sr.Recognizer()
 
-openai.api_key = "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"
+openai.api_key = ""
 prompt= "hello there, in obi wan voice"
 def create_response(text):
   response = openai.Completion.create(
